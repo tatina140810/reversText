ede387bed386c8f97d5bfe0cd5f8c1dcb52b22f3
tatina140810 <tatu140810tatu@gmail.com>
2024-10-08 16:52:05 +0600

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Tatina Dzhakypbekova on 8/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
